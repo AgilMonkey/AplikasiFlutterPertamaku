17c3e95e0a35da5992093c96d4dedd7424b35362
Aepu <muhammadagilfadilah@gmail.com>
2024-03-30 16:55:48 +0800

    first commit dan selesai praktikum 1&2

---
+++ lib/row_widget.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class RowWidget extends StatelessWidget {
+  const RowWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Widget Row')),
+      body: const Row(
+        children: [
+          Text('Kolom 1'),
+          Text('Kolom 2'),
+          Text('Kolom 3'),
+          Text('Kolom 4'),
+        ],
+      ),
+    );
+  }
+}
